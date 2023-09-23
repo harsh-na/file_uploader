52749aaa80cb2a304493b37d67955d20d5f7e044
Harshit <harshitgalaxy710@gmail.com>
2023-09-23 21:01:14 +0530

    back2commit

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -13,7 +13,7 @@ export const uploadImage = async (request, response) => {
     const file = await File.create(fileObj);
     response
       .status(200)
-      .json({ path: `http://localhost:${process.env.PORT}/file/${file._id}` });
+      .json({ path: `https://adee-117-55-241-48.ngrok.io` });
   } catch (error) {
     console.error(error.message);
     response.status(500).json({ error: error.message });
